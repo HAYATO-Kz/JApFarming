bf1fc7838dd6bffe97f91220a92c95d75fe4110a
Hayato <hokiz.1999@gmail.com>
2021-02-26 07:34:15 +0700

    Updates

---
+++ precache-manifest.cd537d8830d6a57320cda5b1c2ab6625.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0f7adccdc614c8f4b47",
+    "url": "/JApFarming/static/css/main.e3168520.chunk.css"
+  },
+  {
+    "revision": "f0f7adccdc614c8f4b47",
+    "url": "/JApFarming/static/js/main.f0f7adcc.chunk.js"
+  },
+  {
+    "revision": "de735eb047fc6aa38a53",
+    "url": "/JApFarming/static/css/1.e08f339f.chunk.css"
+  },
+  {
+    "revision": "de735eb047fc6aa38a53",
+    "url": "/JApFarming/static/js/1.de735eb0.chunk.js"
+  },
+  {
+    "revision": "48ba251ef280e7ecba30",
+    "url": "/JApFarming/static/js/runtime~main.48ba251e.js"
+  },
+  {
+    "revision": "985b027744f8c538e71aa776ca65b6b4",
+    "url": "/JApFarming/index.html"
+  }
+];
